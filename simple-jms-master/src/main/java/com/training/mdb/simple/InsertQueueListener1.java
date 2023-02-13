4124bfae3634690bca99219a6af363ba64d5a1fc
Syihanghifari <syihanghifari27@gmail.com>
2023-02-13 16:31:59 +0700

    tambah

---
+++ simple-jms-master/src/main/java/com/training/mdb/simple/InsertQueueListener1.java
@@ -0,0 +1,39 @@
+package com.training.mdb.simple;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue="javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "connectionFactoryJndiName", propertyValue="jms.TrainingCF"),
+		@ActivationConfigProperty(propertyName = "destinationJndiName", propertyValue="jms.InsertNasabah")
+}, mappedName="jms.InsertNasabah")
+public class InsertQueueListener1 implements MessageListener{
+	
+	public void onMessage(Message message){
+		
+		try{
+			String messageInString = "";
+			if (message instanceof TextMessage){
+				messageInString = ((TextMessage) message).getText();
+			} 
+			String[] datas =  messageInString.split(" ");
+			for (String data : datas) {
+				System.out.println(data);
+			}
+			try{
+				Thread.sleep(5000l);
+				System.out.println("Process finished instance 1");
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}catch(JMSException e){
+			System.err.println("Error " + e.getMessage());
+		}
+	}
+}
